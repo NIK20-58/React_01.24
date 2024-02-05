749adfb5367f6277f51eff71ad87aabe8fefdc8a
Nikita <kus.ai@yandex.ru>
2024-02-05 13:13:50 +0400

    timer

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from 'react'
+
 export const Timer = () => {
-  return <p>Time: 2:00</p>
+  const [time, setTime] = useState(150)
+
+  const minutes = Math.floor(time / 60)
+    .toString()
+    .padStart(2, '0')
+  const seconds = (time % 60).toString().padStart(2, '0')
+
+  // setInterval(() => {
+  //   setTime(Math.max(time - 1, 0))
+  // }, 1000)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((t) => Math.max(t - 1, 0))
+    }, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  return (
+    <p>
+      Time: {minutes}:{seconds}
+    </p>
+  )
 }
